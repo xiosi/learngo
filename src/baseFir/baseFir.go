e8a2346722f881c7c4314ce01cb2a3d4e4ceb1fd
xiosi <675290311@qq.com>
2021-10-23 11:30:05 +0800

    init

---
+++ src/baseFir/baseFir.go
@@ -0,0 +1,10 @@
+package baseFir
+
+import "fmt"
+
+func init() {
+	fmt.Println("baseFir init")
+}
+func Todo() {
+	fmt.Println("invove baseFir")
+}
---
+++ src/baseSecon/init.go
@@ -0,0 +1,14 @@
+package baseSecon
+
+import (
+	"fmt"
+	// base "./baseFir"
+)
+
+func init() {
+	fmt.Println("baseSecon init")
+	// base.Todo()
+}
+func Todo() {
+	fmt.Println("invove baseSecon")
+}
